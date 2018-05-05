c40f5f801531396c0322a3e71381e386be83c438
CSC <csc.1994@qq.com>
2018-05-05 21:56:27 +0800

    更新

--- src/main/java/qdu/graduation/backend/services/TeacherInfoService.java
+++ src/main/java/qdu/graduation/backend/services/TeacherInfoService.java
@@ -3,7 +3,6 @@ package qdu.graduation.backend.services;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.sun.xml.internal.ws.api.message.Packet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -74,7 +73,7 @@ public class TeacherInfoService {
         }
     }
 
-        public String getNews() {
+    public String getNews() {
         try {
             JSONObject res = JSON.parseObject(StatusCode.success.toString());
             res.put("news", newsDao.latestNew());
@@ -85,7 +84,7 @@ public class TeacherInfoService {
         }
     }
 
-    public String getNewsById(Integer newsId){
+    public String getNewsById(Integer newsId) {
         try {
             JSONObject res = JSON.parseObject(StatusCode.success.toString());
             res.put("news", newsDao.selectByPrimaryKey(newsId));
